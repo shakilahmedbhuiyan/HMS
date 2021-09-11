954b4016715c34eebabe8b9409a6ffa35d9409c6
shakilahmedbhuiyan <16103107@iubat.edu>
2021-09-12 00:40:18 +0600

    Entity Framework added

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,25 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+
+    {
+        protected HMSContext() : base("HMSConnectionString")
+        {
+
+        }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
