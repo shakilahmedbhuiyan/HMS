ef45e5191fc9ce25ff5784d78cc335f5b8800929
shakilahmedbhuiyan <16103107@iubat.edu>
2021-09-11 14:28:07 +0600

    Added Basic Entities

---
+++ HMS.Entities/AccomodationPackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class AccomodationPackage
+    {
+        public int Id { get; set; }
+        public AccomodationType AccomodationTypeId { get; set; }
+        public string Name { get; set; }
+        public int NoOfRoom { get; set; }
+        public decimal FeePerNight { get; set; }
+    }
+}
---
+++ HMS.Entities/AccomodationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class AccomodationType
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+
+    }
+}
